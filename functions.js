f2f934a75ea7ccbef1037cc291a546d4c0da7a82
Ryan Nelson <ryannelson881995@gmail.com>
2018-08-25 11:49:14 -0500

    functions js

--- functions.js
+++ functions.js
@@ -22,3 +22,23 @@ const bearSticks = nuggetizer('bear');
 console.log(nomnom('ryan', bearSticks)); 
 
 
+const numberadder = (num) => {
+    const finalNumber = num + 3;
+    printToDom(`<h2>${finalNumber}</h2>`, 'allTheNumbers');
+
+};
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += stringToPrint;
+};
+
+// console.log(`num: `, numberadder(22));
+numberadder(22)
+numberadder(2)
+numberadder(27)
+numberadder(44)
+
+
+
+printToDom('')
