7fa3536ad07036f701230ac01499f205ea1581dd
Ryan Nelson <ryannelson881995@gmail.com>
2018-08-25 10:18:05 -0600

    h js

---
+++ functions.js
@@ -0,0 +1,24 @@
+// input = animal ex. fishoutput = 
+// output = animal product ex. fish stix
+
+function nuggetizer(animal){
+    return `${animal} stix`;
+};
+console.log('fish:', nuggetizer('fish'));
+console.log('cat:', nuggetizer('cat'));
+console.log('bear:', nuggetizer('bear'));
+
+
+const nuggetizer2 = (animal) => {
+    return `${animal} jerky`
+};
+console.log('deer:', nuggetizer2('deer'));
+const nomnom = (person, food) => {
+    return `${person} devoured ${food}`;
+}
+
+const bearSticks = nuggetizer('bear');
+
+console.log(nomnom('ryan', bearSticks)); 
+
+
